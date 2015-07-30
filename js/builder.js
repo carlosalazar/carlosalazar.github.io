0a0e8002acb506174d07983fa060f1fcfe0d965d
Carlos Salazar <jcarlos.slz@gmail.com>
2015-07-30 16:59:42 -0500

    Added builder.js Changed icons from Google to Bootstrap

---
+++ js/builder.js
@@ -0,0 +1,53 @@
+$(document).ready( function () {
+    var model = {
+        skills: ['HTML', 'Javascript', 'JQuery', 'AngularJS', 'CSS', 'Java'],
+        languages: ['Spanish', 'English'],
+        softskills: ['Self taught', 'Team player'],
+        hobbies: ['Reading (books and tech news)', 'Listen to music', 'Movies & Series']
+    };
+
+    var view = {
+        init: function () {
+            var skillList = $('#skills-list');
+            var languageList = $('#languages-list');
+            var softSkillList = $('#softskills-list');
+            var hobbieList = $('#hobbies-list');
+
+            $.each(controller.getSkills(), function (key, value) {
+                $('<li>' + value +'</li>').appendTo(skillList);
+            });
+
+            $.each(controller.getLanguages(), function (key, value) {
+                $('<li>' + value +'</li>').appendTo(languageList);
+            });
+
+            $.each(controller.getSoftSkills(), function (key, value) {
+                $('<li>' + value +'</li>').appendTo(softSkillList);
+            });
+
+            $.each(controller.getHobbies(), function (key, value) {
+                $('<li>' + value +'</li>').appendTo(hobbieList);
+            });
+        }
+    };
+
+    var controller = {
+        init: function () {
+            view.init();
+        },
+        getSkills: function () {
+            return model.skills;
+        },
+        getLanguages: function () {
+            return model.languages;
+        },
+        getSoftSkills: function () {
+            return model.softskills;
+        },
+        getHobbies: function () {
+            return model.hobbies;
+        }
+    };
+
+    controller.init();
+});
