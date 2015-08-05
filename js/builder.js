13f54cbb4fab816c844acabb760bc642dbfe133c
Carlos Salazar <jcarlos.slz@gmail.com>
2015-08-05 16:10:31 -0500

    Added jobs list to builder.

--- js/builder.js
+++ js/builder.js
@@ -3,30 +3,53 @@ $(document).ready(function () {
         skills: ['Android', 'HTML', 'Javascript', 'jQuery', 'AngularJS'],
         languages: ['Spanish', 'English'],
         softskills: ['Self taught', 'Team player'],
-        hobbies: ['Reading (books and tech news)', 'Listen to music', 'Movies & series']
+        hobbies: ['Reading (books and tech news)', 'Listen to music', 'Movies & series'],
+        jobs: [
+            {
+                title: 'Intern',
+                company: 'Sanmina',
+                startDate: '2014',
+                endDate: '2015',
+                description: 'Lorem ipsum'
+            },
+            {
+                title: 'Programmer Analyst',
+                company: 'Sanmina',
+                startDate: '2015',
+                endDate: 'Present',
+                description: 'AngularJS and Android developer.'
+            }
+        ]
     };
 
     var view = {
         init: function () {
-            var skillList = $('#skills-list');
-            var languageList = $('#languages-list');
-            var softSkillList = $('#softskills-list');
-            var hobbieList = $('#hobbies-list');
+            var skillsList = $('#skills-list');
+            var languagesList = $('#languages-list');
+            var softSkillsList = $('#softskills-list');
+            var hobbiesList = $('#hobbies-list');
+            var jobsList = $('#jobs-list');
 
             $.each(controller.getSkills(), function (key, value) {
-                $('<li>' + value + '</li>').appendTo(skillList);
+                $('<li>' + value + '</li>').appendTo(skillsList);
             });
 
             $.each(controller.getLanguages(), function (key, value) {
-                $('<li>' + value + '</li>').appendTo(languageList);
+                $('<li>' + value + '</li>').appendTo(languagesList);
             });
 
             $.each(controller.getSoftSkills(), function (key, value) {
-                $('<li>' + value + '</li>').appendTo(softSkillList);
+                $('<li>' + value + '</li>').appendTo(softSkillsList);
             });
 
             $.each(controller.getHobbies(), function (key, value) {
-                $('<li>' + value + '</li>').appendTo(hobbieList);
+                $('<li>' + value + '</li>').appendTo(hobbiesList);
+            });
+
+            $.each(controller.getJobs(), function (key, value) {
+                $('<div class="col-lg-2"><p>' + value.startDate + ' - ' + value.endDate + '</p></div>').appendTo(jobsList);
+                $('<div class="col-lg-10"><p><b>' + value.company + '</b> - ' + value.title + '</p><p></p></div>').appendTo(jobsList);
+                $('<div class="col-lg-10 col-lg-offset-2"><p>' + value.description + '</p></div>').appendTo(jobsList);
             });
 
             view.firstTimeRender();
@@ -51,6 +74,9 @@ $(document).ready(function () {
         },
         getHobbies: function () {
             return model.hobbies;
+        },
+        getJobs: function () {
+            return model.jobs;
         }
     };
 
